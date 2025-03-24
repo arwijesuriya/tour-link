2df5ef5ea8e4ab10d335e35a00a0770612275956
Achira Wijesuriya <wijesuriya4me@gmail.com>
2025-03-25 01:47:45 +0530

    Update Routers.js

--- frontend/src/router/Routers.js
+++ frontend/src/router/Routers.js
@@ -1,10 +1,24 @@
 import React from 'react'
+import {Routes, Route, Navigate} from 'react-router-dom'
+
+import Home from './../pages/Home';
+import Tours from './../pages/Tours';
+import TourDeatails from './../pages/TourDeatails';
+import Login from './../pages/Login';
+import Register from './../pages/Register';
+import SearchResultDetails from './../pages/SearchResultDetails';
 
 const Routers = () => {
   return (
-    <div>
-      
-    </div>
+    <Routes>
+        <Route path="/" element={<Navigate to = '/home' />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/tours" element={<Tours />} />
+        <Route path="/tours/:id" element={<TourDeatails />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/tours/search" element={<SearchResultDetails />} />
+    </Routes>
   )
 }
 
