efe4e57748ba48a3691ed4ef96ed85614a18d658
Achira Wijesuriya <wijesuriya4me@gmail.com>
2025-03-25 23:45:57 +0530

    featured text section

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,14 +1,15 @@
-import React from 'react'
-import '../styles/home.css'
+import React from "react";
+import "../styles/home.css";
 
-import { Container, Row, Col } from 'reactstrap'
-import heroImg from '../assets/images/hero-img01.jpg'
-import heroImg02 from '../assets/images/hero-img02.jpg'
-import heroVideo from '../assets/images/hero-video.mp4'
-import Subtitle from './../shared/Subtitle'
-import worldImg from '../assets/images/world.png'
+import { Container, Row, Col } from "reactstrap";
+import heroImg from "../assets/images/hero-img01.jpg";
+import heroImg02 from "../assets/images/hero-img02.jpg";
+import heroVideo from "../assets/images/hero-video.mp4";
+import Subtitle from "./../shared/Subtitle";
+import worldImg from "../assets/images/world.png";
 
-import SearchBar from '../shared/SearchBar'
+import SearchBar from "../shared/SearchBar";
+import ServiceList from "../services/ServiceList";
 
 const Home = () => {
   return (
@@ -17,44 +18,43 @@ const Home = () => {
       <section>
         <Container>
           <Row>
-            <Col lg='6'>
+            <Col lg="6">
               <div className="hero__content">
                 <div className="hero__subtitle d-flex align-items-center">
-                  <Subtitle Subtitle={'Know Before You Go'} />
-                  <img src={worldImg} alt='' />
+                  <Subtitle Subtitle={"Know Before You Go"} />
+                  <img src={worldImg} alt="" />
                 </div>
                 <h1>
                   Traveling opens the door to creating{" "}
-                  <span className='highlight'>memories</span>
+                  <span className="highlight">memories</span>
                 </h1>
                 <p>
-                  Experience the world through our extensive tours, allowing you to
-                  immerse yourself in the diverse cultures, traditions, and customs of
-                  our beautiful cities.
+                  Experience the world through our extensive tours, allowing you
+                  to immerse yourself in the diverse cultures, traditions, and
+                  customs of our beautiful cities.
                 </p>
               </div>
             </Col>
 
-            <Col lg='2'>
+            <Col lg="2">
               <div className="hero__img-box">
-                <img src={heroImg} alt='' />
+                <img src={heroImg} alt="" />
               </div>
             </Col>
 
-            <Col lg='2'>
+            <Col lg="2">
               <div className="hero__img-box mt-4">
-                <video src={heroVideo} alt='' controls />
+                <video src={heroVideo} alt="" controls />
               </div>
             </Col>
 
-            <Col lg='2'>
+            <Col lg="2">
               <div className="hero__img-box mt-5">
-                <img src={heroImg02} alt='' />
+                <img src={heroImg02} alt="" />
               </div>
             </Col>
 
             <SearchBar />
-
           </Row>
         </Container>
       </section>
@@ -63,15 +63,31 @@ const Home = () => {
       <section>
         <Container>
           <Row>
-            <Col lg='3'>
-              <h5 className='services__subtitle'>What we serve</h5>
-              <h5 className='services__title'>We offer our best services</h5>
+            <Col lg="3">
+              <h5 className="services__subtitle">What we serve</h5>
+              <h5 className="services__title">We offer our best services</h5>
             </Col>
+
+            <ServiceList />
           </Row>
         </Container>
       </section>
-    </>
-  )
-}
 
-export default Home
+      {/* Featured section start */}
+        <Container>
+          <Row>
+            <Col lg='12' className='mb-5'>
+              <Subtitle Subtitle={'Explore'} />
+              <h2 className='featured__tour-title'>
+                Discover the world's top destinations with our exclusive travel packages
+              </h2>
+            </Col>
+          </Row>
+        </Container>
+      {/* Featured section end */}
+
+    </>
+  );
+};
+
+export default Home;
--- frontend/src/services/ServiceList.jsx
+++ frontend/src/services/ServiceList.jsx
@@ -1,11 +1,39 @@
 import React from 'react'
+import ServiceCard from './ServiceCard'
+import { Col } from 'reactstrap'
+
+import weatherImg from '../assets/images/weather.png'
+import guideImg from '../assets/images/guide.png'
+import customizationImg from '../assets/images/customization.png'
+
+const servicesData = [
+    {
+        imgUrl: weatherImg,
+        title: 'Calculate weather',
+        desc: 'Get the weather forecast for your destination.'
+    },
+    {
+        imgUrl: guideImg,
+        title: 'Best Tour Guide',
+        desc: 'Hire a tour guide for your trip.'
+    },
+    {
+        imgUrl: customizationImg,
+        title: 'Customization',
+        desc: 'Customize your trip.'
+    }
+]
 
 const ServiceList = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+    return (
+        <>
+            {servicesData.map((item, index) => (
+                <Col lg='3' key={index}>
+                    <ServiceCard item={item} />
+                </Col>
+            ))}
+        </>
+    )
 }
 
 export default ServiceList
